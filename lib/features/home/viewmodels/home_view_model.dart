32ebc6a95fb279f7b71092e0032aad53e492173d
alexsandersarmento <alexsander.br18@gmail.com>
2025-02-26 20:40:42 -0300

    feat: implement product fetching logic in HomeViewModel

--- lib/features/home/viewmodels/home_view_model.dart
+++ lib/features/home/viewmodels/home_view_model.dart
@@ -1,3 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:vizinhanca_shop/data/models/product_model.dart';
+import 'package:vizinhanca_shop/data/repositories/product_repository.dart';
 
-class HomeViewModel extends ChangeNotifier {}
+class HomeViewModel extends ChangeNotifier {
+  final ProductRepository _repository;
+  List<ProductModel> _products = [];
+  bool _isLoading = false;
+
+  HomeViewModel({required ProductRepository repository})
+    : _repository = repository;
+
+  List<ProductModel> get products => _products;
+  bool get isLoading => _isLoading;
+
+  void handleGetProducts() async {
+    _isLoading = true;
+    notifyListeners();
+
+    try {
+      _products = await _repository.fetchProducts();
+    } catch (e) {
+      _products = [];
+    } finally {
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
+}
