ffc001ed5078792ebc965c8cb38d3393435e35a7
alexsandersarmento <alexsander.br18@gmail.com>
2025-03-01 16:18:23 -0300

    feat: improve authentication handling and UI updates in MyAnnouncementsView

--- lib/features/my_announcement/views/my_announcements_view.dart
+++ lib/features/my_announcement/views/my_announcements_view.dart
@@ -25,11 +25,13 @@ class MyAnnouncementsView extends StatefulWidget {
 class _MyAnnouncementsViewState extends State<MyAnnouncementsView> {
   final TextEditingController _searchController = TextEditingController();
   bool _isLoggedIn = false;
+  bool _isCheckingAuthStatus = true;
 
   void _checkAuthStatus() async {
     final isLoggedIn = await widget.authService.isUserLoggedIn();
     setState(() {
       _isLoggedIn = isLoggedIn;
+      _isCheckingAuthStatus = false;
     });
 
     if (isLoggedIn) {
@@ -57,36 +59,30 @@ class _MyAnnouncementsViewState extends State<MyAnnouncementsView> {
 
   @override
   Widget build(BuildContext context) {
-    return Builder(
-      builder: (context) {
-        if (!_isLoggedIn) {
-          return Scaffold(
-            backgroundColor: Colors.white,
-            body: Login(
-              title: 'Faça login para ver seus anúncios',
-              message: 'Você precisa estar logado para\nacessar seus anúncios',
-            ),
-          );
-        }
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text(
+          'Meus Anúncios',
+          style: GoogleFonts.sora(
+            fontSize: 16,
+            fontWeight: FontWeight.w500,
+            color: Colors.grey[800],
+          ),
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leadingWidth: MediaQuery.of(context).size.width,
+        toolbarHeight: 80,
+        scrolledUnderElevation: 0,
+        actions: [
+          Builder(
+            builder: (context) {
+              if (!_isLoggedIn) {
+                return const SizedBox();
+              }
 
-        return Scaffold(
-          backgroundColor: Colors.white,
-          appBar: AppBar(
-            title: Text(
-              'Meus Anúncios',
-              style: GoogleFonts.sora(
-                fontSize: 16,
-                fontWeight: FontWeight.w500,
-                color: Colors.grey[800],
-              ),
-            ),
-            backgroundColor: Colors.white,
-            elevation: 0,
-            leadingWidth: MediaQuery.of(context).size.width,
-            toolbarHeight: 80,
-            scrolledUnderElevation: 0,
-            actions: [
-              IconButton(
+              return IconButton(
                 onPressed: _navigateToNewAnnouncement,
                 icon: Container(
                   margin: const EdgeInsets.only(right: 16),
@@ -98,126 +94,132 @@ class _MyAnnouncementsViewState extends State<MyAnnouncementsView> {
                   child: const Icon(Icons.add_rounded, size: 32),
                 ),
                 color: Colors.white,
-              ),
-            ],
+              );
+            },
           ),
-          body: Padding(
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              children: [
-                TextField(
-                  controller: _searchController,
-                  decoration: InputDecoration(
-                    prefixIcon: const Icon(Icons.search, color: Colors.grey),
-                    hintText: 'Buscar nos meus anúncios',
-                    hintStyle: GoogleFonts.sora(color: Colors.grey[600]),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Colors.grey[300]!,
-                        width: 1,
-                      ),
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Colors.grey[300]!,
-                        width: 1,
-                      ),
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
-                    contentPadding: const EdgeInsets.symmetric(
-                      vertical: 14.0,
-                      horizontal: 16.0,
-                    ),
-                    filled: true,
-                    fillColor: Colors.grey[100],
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            if (_isLoggedIn)
+              TextField(
+                controller: _searchController,
+                decoration: InputDecoration(
+                  prefixIcon: const Icon(Icons.search, color: Colors.grey),
+                  hintText: 'Buscar nos meus anúncios',
+                  hintStyle: GoogleFonts.sora(color: Colors.grey[600]),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey[300]!, width: 1),
+                    borderRadius: BorderRadius.circular(8.0),
                   ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey[300]!, width: 1),
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  contentPadding: const EdgeInsets.symmetric(
+                    vertical: 14.0,
+                    horizontal: 16.0,
+                  ),
+                  filled: true,
+                  fillColor: Colors.grey[100],
                 ),
-                ListenableBuilder(
-                  listenable: widget.viewModel,
-                  builder: (context, snapshot) {
-                    if (widget.viewModel.isLoading) {
-                      return Expanded(
-                        child: const Center(
-                          child: CircularProgressIndicator(
-                            valueColor: AlwaysStoppedAnimation<Color>(
-                              AppColors.primary,
-                            ),
-                          ),
+              ),
+            ListenableBuilder(
+              listenable: widget.viewModel,
+              builder: (context, snapshot) {
+                if (widget.viewModel.isLoading || _isCheckingAuthStatus) {
+                  return Expanded(
+                    child: const Center(
+                      child: CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation<Color>(
+                          AppColors.primary,
                         ),
-                      );
-                    }
+                      ),
+                    ),
+                  );
+                }
 
-                    if (widget.viewModel.announcements.isEmpty &&
-                        _searchController.text.isEmpty) {
-                      return Expanded(
-                        child: Center(
-                          child: Text(
-                            'Nenhum anúncio cadastrado',
-                            style: GoogleFonts.sora(
-                              fontSize: 16,
-                              color: Colors.grey[800],
-                            ),
-                          ),
+                if (!_isLoggedIn) {
+                  return Expanded(
+                    child: Transform.translate(
+                      offset: Offset(0, -100),
+                      child: Login(
+                        title: 'Faça login para ver seus anúncios',
+                        message:
+                            'Você precisa estar logado para\nacessar seus anúncios',
+                      ),
+                    ),
+                  );
+                }
+
+                if (widget.viewModel.announcements.isEmpty &&
+                    _searchController.text.isEmpty) {
+                  return Expanded(
+                    child: Center(
+                      child: Text(
+                        'Nenhum anúncio cadastrado',
+                        style: GoogleFonts.sora(
+                          fontSize: 16,
+                          color: Colors.grey[800],
                         ),
-                      );
-                    }
+                      ),
+                    ),
+                  );
+                }
 
-                    if (widget.viewModel.filteredAnnouncements.isEmpty &&
-                        _searchController.text.isNotEmpty) {
-                      return SizedBox(
-                        height: 200,
-                        child: Center(
-                          child: Text(
-                            'Nenhum anúncio encontrado',
-                            style: GoogleFonts.sora(
-                              fontSize: 16,
-                              color: Colors.grey[800],
-                            ),
-                          ),
+                if (widget.viewModel.filteredAnnouncements.isEmpty &&
+                    _searchController.text.isNotEmpty) {
+                  return SizedBox(
+                    height: 200,
+                    child: Center(
+                      child: Text(
+                        'Nenhum anúncio encontrado',
+                        style: GoogleFonts.sora(
+                          fontSize: 16,
+                          color: Colors.grey[800],
                         ),
-                      );
-                    }
+                      ),
+                    ),
+                  );
+                }
 
-                    return Expanded(
-                      child: SingleChildScrollView(
-                        child: Column(
-                          children: [
-                            const SizedBox(height: 16),
-                            ListView.builder(
-                              shrinkWrap: true,
-                              physics: const NeverScrollableScrollPhysics(),
-                              itemCount:
-                                  widget.viewModel.filteredAnnouncements.length,
-                              itemBuilder: (context, index) {
-                                final announcement =
-                                    widget
-                                        .viewModel
-                                        .filteredAnnouncements[index];
-                                return MyAnnouncementPreview(
-                                  announcement: announcement,
-                                  onTap: () {
-                                    Navigator.of(context).pushNamed(
-                                      AppRoutes.myAnnouncementDetails,
-                                      arguments: MyAnnouncementDetailsArguments(
-                                        announcementId: announcement.id,
-                                      ),
-                                    );
-                                  },
+                return Expanded(
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        const SizedBox(height: 16),
+                        ListView.builder(
+                          shrinkWrap: true,
+                          physics: const NeverScrollableScrollPhysics(),
+                          itemCount:
+                              widget.viewModel.filteredAnnouncements.length,
+                          itemBuilder: (context, index) {
+                            final announcement =
+                                widget.viewModel.filteredAnnouncements[index];
+                            return MyAnnouncementPreview(
+                              announcement: announcement,
+                              onTap: () {
+                                Navigator.of(context).pushNamed(
+                                  AppRoutes.myAnnouncementDetails,
+                                  arguments: MyAnnouncementDetailsArguments(
+                                    announcementId: announcement.id,
+                                  ),
                                 );
                               },
-                            ),
-                          ],
+                            );
+                          },
                         ),
-                      ),
-                    );
-                  },
-                ),
-              ],
+                      ],
+                    ),
+                  ),
+                );
+              },
             ),
-          ),
-        );
-      },
+          ],
+        ),
+      ),
     );
   }
 }
