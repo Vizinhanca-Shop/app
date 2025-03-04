431422e59734e9fdbbc02809e46d7c6013aeb5dd
alexsandersarmento <alexsander.br18@gmail.com>
2025-03-04 17:12:32 -0300

    feat: add optional search parameter to FiltersModel for enhanced filtering

--- lib/data/models/filters_model.dart
+++ lib/data/models/filters_model.dart
@@ -2,11 +2,22 @@ class FiltersModel {
   final double radius;
   final String order;
   final String category;
+  final String? search;
 
-  FiltersModel({required this.radius, required this.order, this.category = ''});
+  FiltersModel({
+    required this.radius,
+    required this.order,
+    this.category = '',
+    this.search,
+  });
 
   Map<String, dynamic> toJson() {
-    return {'radius': radius, 'order': order, 'category': category};
+    return {
+      'radius': radius,
+      'order': order,
+      'category': category,
+      'search': search,
+    };
   }
 
   factory FiltersModel.fromJson(Map<String, dynamic> json) {
@@ -14,10 +25,11 @@ class FiltersModel {
       radius: json['radius'],
       order: json['order'],
       category: json['category'],
+      search: json['search'],
     );
   }
 
   factory FiltersModel.empty() {
-    return FiltersModel(radius: 5, order: 'asc', category: '');
+    return FiltersModel(radius: 5, order: 'asc', category: '', search: '');
   }
 }
