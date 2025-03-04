dbe1216ccca26268b00f942343022feb2df9818a
alexsandersarmento <alexsander.br18@gmail.com>
2025-03-03 22:05:18 -0300

    feat: add default categories for the application

---
+++ lib/constants/categories.dart
@@ -0,0 +1,21 @@
+import 'package:vizinhanca_shop/data/models/category_model.dart';
+
+final List<CategoryModel> defaultCategories = [
+  CategoryModel(id: '67c490caf9b866b6e221de0f', name: 'Alimentos e Bebidas'),
+  CategoryModel(
+    id: '67c490caf9b866b6e221de12',
+    name: 'Eletrônicos e Acessórios',
+  ),
+  CategoryModel(id: '67c490caf9b866b6e221de15', name: 'Moda e Acessórios'),
+  CategoryModel(id: '67c490caf9b866b6e221de18', name: 'Casa e Decoração'),
+  CategoryModel(id: '67c490cbf9b866b6e221de1b', name: 'Serviços'),
+  CategoryModel(
+    id: '67c490cbf9b866b6e221de1e',
+    name: 'Artesanato e Personalizados',
+  ),
+  CategoryModel(id: '67c490cbf9b866b6e221de21', name: 'Infantil'),
+  CategoryModel(id: '67c490cbf9b866b6e221de24', name: 'Automotivo'),
+  CategoryModel(id: '67c490cbf9b866b6e221de27', name: 'Esportes e Lazer'),
+  CategoryModel(id: '67c490cbf9b866b6e221de2a', name: 'Beleza e Estética'),
+  CategoryModel(id: '67c490cbf9b866b6e221de2d', name: 'Outros'),
+];
