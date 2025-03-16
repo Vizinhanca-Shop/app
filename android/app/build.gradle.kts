c8d6853c39e008a7bdb32bc89ab9ee1d7c20870f
alexsandersarmento <alexsander.br18@gmail.com>
2025-03-16 14:44:07 -0300

    refactor: replace core-ktx dependency with app-update and app-update-ktx version 2.1.0 in build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -62,7 +62,8 @@ android {
 }
 
 dependencies {
-    implementation("com.google.android.play:core-ktx:1.8.1")
+    implementation("com.google.android.play:app-update:2.1.0")
+    implementation("com.google.android.play:app-update-ktx:2.1.0")
 }
 
 
